1879efb9b9a2481b6246d68b354bebe3345ed801
Miguel Centellas <163223596+reyrojo2@users.noreply.github.com>
2025-10-04 15:31:39 +0000

    Initialize project-cultivarium with Vite setup, add SVG assets, and implement counter functionality

--- project-cultivarium/src/main.js
+++ project-cultivarium/src/main.js
@@ -1,6 +1,6 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/Cultivarium.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
